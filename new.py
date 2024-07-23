862b4671d68b51ba3077989264c8b1a07d15a8c9
Aleksey <bizukin@yandex.ru>
2024-07-23 16:45:37 +0300

    7

--- new.py
+++ new.py
@@ -35,6 +35,29 @@ def init_db():
     conn.commit()
     conn.close()
 
+init_db()
+
+@dp.message(CommandStart())
+async def start(message: Message, state: FSMContext):
+    await message.answer("Привет! Как тебя зовут?")
+    await state.set_state(Form.name)
+
+@dp.message(Form.name)
+async def name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer("Сколько тебе лет?")
+    await state.set_state(Form.age)
+
+@dp.message(Form.age)
+async def age(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer("Из какого ты города?")
+    await state.set_state(Form.city)
+
+@dp.message(Form.city)
+async def city(message: Message, state: FSMContext):
+    await state.update_data(city=message.text)
+    user_data = await state.get_data()
 
 
 
