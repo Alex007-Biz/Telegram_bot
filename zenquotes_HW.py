12e7e78af832d5e5be90d46c2edf8f4d8f76314d
Aleksey <bizukin@yandex.ru>
2024-08-02 14:20:00 +0300

    quotes

--- zenquotes_HW.py
+++ zenquotes_HW.py
@@ -2,34 +2,45 @@ import asyncio
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, FSInputFile, CallbackQuery
-# from gtts import gTTS
-# import os
 from config2 import TOKEN
-# import keyboards as kb
-import random
 import requests
 # from datetime import datetime, timedelta
 # import aiohttp
-
+# from gtts import gTTS
+# import os
+# import keyboards as kb
+# import random
+from googletrans import Translator
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+translator = Translator()
 
 def get_quote():
     url = f'https://zenquotes.io/api/random'
     response = requests.get(url)
     return response.json()
 
-# q = get_quote()
-# print(q)
+# Перевод текста
+def translate_text(text, target_language='ru'):
+    try:
+        translated = translator.translate(text, dest=target_language)
+        return translated.text
+    except Exception as e:
+        print(f"Ошибка при переводе: {e}")
+        return text  # Возвращаем исходный текст в случае ошибки
+
+# trr = translate_text('Hello world')
+# print(trr)
 
 @dp.message(Command("quote"))
 async def quote(message: Message):
    new_quote = get_quote()
    quote_text = new_quote[0]['q']
+   translated_quote = translate_text(quote_text)
    author = new_quote[0]['a']
 
-   await message.answer(f'"{quote_text}" ({author})\n')
+   await message.answer(f'"{translated_quote}" ({author})\n')
 
 @dp.message(CommandStart())
 async def start(message: Message):
