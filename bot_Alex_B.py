a6056d5c8fb2d353875225665431b7cc4d948a1b
Alex007-Biz <bizukin@yandex.ru>
2024-09-10 19:45:32 +0300

    1

--- bot_Alex_B.py
+++ bot_Alex_B.py
@@ -15,50 +15,50 @@ from aiogram.fsm.storage.memory import MemoryStorage
 import aiohttp
 
 
-bot = Bot(token=TOKEN)
+bot = Bot(token=TOKEN_BOT_ALEX_B)
 dp = Dispatcher()
 
 logging.basicConfig(level=logging.INFO)
 
-button_registr = KeyboardButton(text="Регистрация в телеграм-боте")
 button_exhange_rates = KeyboardButton(text="Курс валют")
-button_tips = KeyboardButton(text="Советы по экономии")
-button_finances = KeyboardButton(text="Личные финансы")
+button_Nasa = KeyboardButton(text="Фото НАСА")
+button_weather = KeyboardButton(text="Погода")
+button_Thoughts = KeyboardButton(text="Философские мысли")
 
 keyboards = ReplyKeyboardMarkup(keyboard=[
-    [button_registr, button_exhange_rates],
-    [button_tips, button_finances]
+    [button_exhange_rates, button_Nasa],
+    [button_weather, button_Thoughts]
     ], resize_keyboard=True)
 
-conn = sqlite3.connect('users.db')
-cursor = conn.cursor()
-
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
-    telegram_id INTEGER UNIQUE,
-    name TEXT,
-    category1 TEXT,
-    category2 TEXT,
-    category3 TEXT,
-    expenses1 REAL,
-    expenses2 REAL,
-    expenses3 REAL
-    )
-''')
-conn.commit()
-
-class FinancesForm(StatesGroup):
-    category1 = State()
-    expenses1 = State()
-    category2 = State()
-    expenses2 = State()
-    category3 = State()
-    expenses3 = State()
+# conn = sqlite3.connect('users.db')
+# cursor = conn.cursor()
+#
+# cursor.execute('''
+# CREATE TABLE IF NOT EXISTS users (
+#     id INTEGER PRIMARY KEY,
+#     telegram_id INTEGER UNIQUE,
+#     name TEXT,
+#     category1 TEXT,
+#     category2 TEXT,
+#     category3 TEXT,
+#     expenses1 REAL,
+#     expenses2 REAL,
+#     expenses3 REAL
+#     )
+# ''')
+# conn.commit()
+#
+# class FinancesForm(StatesGroup):
+#     category1 = State()
+#     expenses1 = State()
+#     category2 = State()
+#     expenses2 = State()
+#     category3 = State()
+#     expenses3 = State()
 
 @dp.message(Command('start'))
 async def send_start(message: Message):
-    await message.answer("Привет! Я ваш личный финансовый помощник. Выберите одну из опций в меню:", reply_markup=keyboards)
+    await message.answer("Приветствую Вас, ! Я ваш личный финансовый помощник. Выберите одну из опций в меню:", reply_markup=keyboards)
 
 @dp.message(F.text == "Регистрация в телеграм-боте")
 async def registration(message: Message):
